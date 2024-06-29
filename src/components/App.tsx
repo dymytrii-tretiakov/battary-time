9879fa35fa33696a8751b722f7438df13ece370b
Dymytrii Tretiakov <dmitriy.tretyakov@digit.az>
2024-06-29 16:38:46 +0300

    chore: Update i18n translations for Japanese language

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -7,12 +7,14 @@ import { initReactI18next } from 'react-i18next';
 import ru from '../locales/ru.json';
 import en from '../locales/en.json';
 import ua from '../locales/ua.json';
+import jp from '../locales/jp.json';
 
 i18next.use(initReactI18next).init({
   resources: {
     en: { translation: en },
     ru: { translation: ru },
-    ua: { translation: ua }
+    ua: { translation: ua },
+    jp: { translation: jp }
   },
   lng: localStorage.getItem('i18nextLng') || 'ua',
   interpolation: {
